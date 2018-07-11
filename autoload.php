e29673507833731c05f703e90b39bb13c5b98dda
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 14:01:21 +0300

    Updated comments.

--- autoload.php
+++ autoload.php
@@ -3,7 +3,7 @@
 /*
  * Messages addon for Bear Framework
  * https://github.com/ivopetkov/messages-bearframework-addon
- * Copyright (c) 2017 Ivo Petkov
+ * Copyright (c) Ivo Petkov
  * Free to use under the MIT license.
  */
 
--- ide-helper.php
+++ ide-helper.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Messages addon for Bear Framework
+ * https://github.com/ivopetkov/messages-bearframework-addon
+ * Copyright (c) Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
 namespace BearFramework {
 
     /**
